66aa9aa45608f5e898f4818fdc6951feff7eed6e
action0709 <action0709@mail.ru>
2023-02-16 11:08:19 +0400

    Задачи 1-3, 4-5, 6-8.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     var dog = 8.0;
     var cat = 3.6;
     var paper=763789;
+    var friend = 19;
+    var frog=3.5;
     System.out.println ("Задачи 1-3:");
         System.out.println (dog);
         System.out.println (cat);
@@ -20,7 +22,38 @@ public class Main {
         System.out.println (cat);
         System.out.println (paper);
 
+        System.out.println ("Задачи 4-5:");
+        System.out.println (friend);
+        friend=friend+2;
+        System.out.println (friend);
+        friend=friend/7;
+        System.out.println (friend);
+        System.out.println (frog);
+        frog=frog*10;
+        System.out.println (frog);
+        frog=frog/3.5;
+        System.out.println (frog);
+        frog=frog+4;
+        System.out.println (frog);
 
+        System.out.println ("Задачи 6-8:");
+
+var weightBoxer1=78.2;
+var weightBoxer2=82.7;
+var communisWeight=weightBoxer1+weightBoxer2;
+        System.out.println (communisWeight);
+        var differentiaWeight =weightBoxer2-weightBoxer1;
+        System.out.println (differentiaWeight);
+        differentiaWeight =weightBoxer2%weightBoxer1;
+        System.out.println (differentiaWeight);
+
+var timeWork = 640;
+var timeWorker=8;
+var workerCompany=timeWork/timeWorker;
+        System.out.println ("Всего работников в компании — "+workerCompany+" человек");
+        workerCompany=workerCompany+94;
+        timeWork=workerCompany*timeWorker;
+        System.out.println ("Если в компании работает "+workerCompany+" человека, то всего "+timeWork+ " часов работы может быть поделено между сотрудниками");
 
     }
 }
